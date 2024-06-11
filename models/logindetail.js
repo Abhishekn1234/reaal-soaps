2d7d4c4b775cb28b80f8d3a157469125d5a00ae7
Abhishekn1234 <abhishekpes123@gmail.com>
2024-06-11 20:27:42 +0530

    first commit

--- models/logindetail.js
+++ models/logindetail.js
@@ -1,10 +1,15 @@
 
 const mongoose = require('mongoose');
 
-const loginDetailSchema = new mongoose.Schema({
-    username: { type: String, required: true },
+const LoginDetailSchema = new mongoose.Schema({
+    mobileNumber: { type: String, required: true },
+    memberId: { type: String, required: true },
+    leftCustomer: { type: Number, required: true },
+    rightCustomer: { type: Number, required: true },
+    amount: { type: Number, required: true },
     loginTime: { type: Date, default: Date.now },
-    rebirth: { type: Boolean, default: false }
+    rebirth: { type: Boolean, required: true },
+    count: { type: Number, required: true }
 });
 
-module.exports = mongoose.model('LoginDetail', loginDetailSchema);
+module.exports = mongoose.model('LoginDetail', LoginDetailSchema);
--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    mobileNumber: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    memberId: { type: String, required: true, unique: true },
+    username: { type: String }
 });
 
 module.exports = mongoose.model('User', UserSchema);
